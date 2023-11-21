a375bdc67b7c969835cf3199b9c338cfdb182a12
Jaciesune <lutasek9@gmail.com>
2023-11-21 12:21:05 +0100

    Added Three Another Tests

--- testy_cpp/test.cpp
+++ testy_cpp/test.cpp
@@ -3,16 +3,30 @@
 
 //vector<int> array = { 1, 5, 15, 97, 20, 6, 4, 16, 54 };
 MergeSort m1;
-vector<int> arr_check1 = {2, 5, 7, 9, 13, 18, 20};
+vector<int> arr_check = {2, 5, 7, 9, 13, 18, 20};
+vector<int> arr_check2 = {-20, -18, -13, -9, -7, -5, -2};
+vector<int> arr = {};
 
 TEST(Merge_sort_test, TestAlreadySortedArr) {
-    vector<int> arr = {2, 18, 5, 9, 13, 20, 7};
-
-
+    arr = {2, 5, 7, 9, 13, 18, 20};
     m1.sort(arr);
-    EXPECT_EQ(arr, arr_check1);
+    EXPECT_EQ(arr, arr_check);
 }
 
 TEST(Merge_sort_test, TestReverseSortedArr) {
+    arr = {20, 18, 13, 9, 7, 5, 2};
+    m1.sort(arr);
+    EXPECT_EQ(arr, arr_check);
+}
 
+TEST(Merge_sort_test, TestRandomNumberInsertedArr) {
+    arr = {5, 13, 2, 18, 7, 20, 9};
+    m1.sort(arr);
+    EXPECT_EQ(arr, arr_check);
+}
+
+TEST(Merge_sort_test, TestNegativeNumbersArr) {
+    arr = {-2, -7, -18, -20, -5, -13, -9};
+    m1.sort(arr);
+    EXPECT_EQ(arr, arr_check2);
 }
